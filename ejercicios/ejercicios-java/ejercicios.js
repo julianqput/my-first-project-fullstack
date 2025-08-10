1444aacf539e322a5aead9bcbe7d01b031c54d67
erlin julian quesada pinzon <quesadajulian39@gmail.com>
2025-08-10 16:19:39 -0500

    feat: Se trabajo con variable modificando su valor inicial

--- ejercicios/ejercicios-java/ejercicios.js
+++ ejercicios/ejercicios-java/ejercicios.js
@@ -1,3 +1,5 @@
+alert("Hello, I'm JWeb");
+
 // ==========================================================================
 //  EJEMPLO 1: Modelando un Usuario
 //  ==========================================================================
@@ -28,6 +30,41 @@ let stockProducto = 25; // Usamos 'let' porque el stock cambiará cuando se vend
 console.log("Producto:",nombreProducto);
 console.log("Precio:",precioProducto);
 console.log("Unidades en stock: ",stockProducto);
+
+
+// --- Ejercicio 1: Modela tu Película Favorita ---
+// 1. Crea una constante `tituloPelicula` y guárdale el nombre de tu película favorita.
+// 2. Crea una constante `anioEstreno` y guárdale el año en que se estrenó.
+// 3. Crea una variable `laHasVisto` (usa 'let') y guárdale `true` si ya la viste, o `false` si no.
+// 4. Muestra cada una de estas variables en la consola.
+
+console.log("\n---Ejercicio 1: Mi pelicula favorita ---");
+
+const tituloPelicula = "El juez";
+const anioEstreno = 2024;
+let laHasVisto = true;
+
+console.log("QuePelicula:",tituloPelicula);
+console.log("QueAniosalio:",anioEstreno);
+console.log("LaHAsVisto:",laHasVisto);
+
+
+// --- Ejercicio 2: Simula una Compra ---
+//  Vamos a usar las variables del producto del Ejemplo 2.
+// 1. Disminuye el stock del producto en 1. (Pista: stockProducto = stockProducto - 1; o la forma corta: stockProducto--;)
+// 2. Aplica un descuento de 25 al precio del producto.
+// 3. Muestra en la consola un mensaje que diga: "Se ha vendido 1 Teclado Mecánico RGB."
+// 4. Muestra el nuevo stock y el nuevo precio en la consola.
+
+console.log("\n---Ejercicio 2: Simular una comptra---");
+
+stockProducto = stockProducto - 1;
+precioProducto = precioProducto - 25;
+
+console.log("Se ha vendido 1 teclado Mecanico RGB");
+//console.log("EL nuevo stock es:",stockProducto, "y el nuevo precio del producto es:",precioProducto); Se puede de esta forma pero es la version antigua
+console.log(`El nuevo stock es: ${stockProducto}, y el nuevo precio del producto es: ${precioProducto}`);//Esta es la versioon mas moderna
+
 
 
 
