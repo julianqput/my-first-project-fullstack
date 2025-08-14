dcb760173a19fd32bd25f3efc170f3fc0297d820
erlin julian quesada pinzon <quesadajulian39@gmail.com>
2025-08-13 20:37:47 -0500

    feat: Agrega carpeta para juego piedra,papel o tijera. Este es el primer mini-proyecto de lógica

---
+++ ejercicios/piedra-papel-tijera/juego.js
@@ -0,0 +1,8 @@
+const opciones =["Piedra","Papel","Tijera"];
+
+const eleccionUsuario = "Piedra";
+
+const indiceAleatorio = Math.floor(Math.random()* 3);
+
+const eleccionComputadora = opciones[indiceAleatorio];
+
