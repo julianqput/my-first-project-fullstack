a4380990526f8b7950c1dda9f9db9a275f301bc5
erlin julian quesada pinzon <quesadajulian39@gmail.com>
2025-09-18 08:54:11 -0500

    feat:se esta trabajando en js

---
+++ ejercicios/generador-colores/colores.js
@@ -0,0 +1,6 @@
+console.log("¡Script del Generador de Colores cargado!");
+
+// ==========================================================================
+//  EJERCICIO 1: GENERADOR DE COLORES RGB ALEATORIOS
+// ==========================================================================
+const body = document.querySelector('body');
