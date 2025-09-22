e35b5f26eb0493f74b537c7d59a650fbcf5ee18e
erlin julian quesada pinzon <quesadajulian39@gmail.com>
2025-09-22 17:18:09 -0500

    feat: el codigo esta completo, se esta buscado el error ya que la pagina no funciona como se epera, los botones no estan sirviendo

--- ejercicios/generador-colores/colores.js
+++ ejercicios/generador-colores/colores.js
@@ -19,8 +19,60 @@ const botonCambiarColor = document.querySelector('btn-cambiar-color');
 //    (Pista: Usa Template Literals: `rgb(${rojo}, ${verde}, ${azul})`).
 
 function generarColorAleatorioRGB(){
-    let rojo;
-    let verde;
-    let azul;
-    
+    const rojo = Math.floor(Math.random()*256);
+    const verde = Math.floor(Math.random()*256);
+    const azul = Math.floor(Math.random()*256);
+    return `rgb(${rojo}, ${verde}, ${azul})`; 
 }
+
+// --- PASO 3: AÑADIR EL EVENTO AL BOTÓN ---
+
+botonCambiarColor.addEventListener('click',function(){
+    const nuevoColor = generarColorAleatorioRGB();
+    body.style.backgroundColor = nuevoColor;
+    displayColorRGB.textContent = nuevoColor;
+});
+
+// ==========================================================================
+//  EJERCICIO 2: SELECCIONAR UN COLOR DE UNA PALETA
+// ==========================================================================
+
+// --- PASO 1: DATOS Y SELECCIÓN DE ELEMENTOS ---
+
+const paletaDeColores = [
+    {
+        nombre : "Rojo Pasión", hex: "#e74c3c"
+    },
+    {
+        nombre : "Azul Océano", hex: "#3498db"
+    },
+    {
+        nombre : "Verde Bosque", hex:"#2ecc71"
+    },
+    {
+        nombre: "Amarillo Sol", hex: "#f1c40f"
+    },
+    {
+        nombre: "Morado Real", hex: "#9b59b6"
+    }
+];
+
+const displayColorPaleta = document.querySelector('#color-display-paleta');
+const botonColorPaleta = document.querySelector('btn-color-paleta');
+
+// --- PASO 2: AÑADIR EL EVENTO AL SEGUNDO BOTÓN ---
+
+// --- PASO 2: AÑADIR EL EVENTO AL SEGUNDO BOTÓN ---
+botonColorPaleta.addEventListener('click', function() {
+    // a. Genera un ÍNDICE aleatorio que sea válido para el array `paletaDeColores`.
+    const indiceAleatorio = Math.floor(Math.random() * paletaDeColores.length);
+
+    // b. Usa ese índice para seleccionar un objeto de color del array.
+    const colorSeleccionado = paletaDeColores[indiceAleatorio];
+
+    // c. Actualiza el color de fondo del `body` usando el código hexadecimal del color.
+    body.style.backgroundColor = colorSeleccionado.hex;
+    
+    // d. Actualiza el texto del `displayColorPaleta` para mostrar el nombre del color.
+    displayColorPaleta.textContent = colorSeleccionado.nombre;
+});
