2a938a2870b6f4b680d74afa34578f7d2e550fa3
erlin julian quesada pinzon <quesadajulian39@gmail.com>
2025-10-07 16:27:57 -0500

    feat: se esta trabajando en el js, se creo el mensaje en la consola, y el array que contiene la descripcion,nombre e imagen del producto, tambien se agrego una carpeta llamada favicon para guardar las imagenes de los productos

---
+++ ejercicios/proyecto-cafeteria/cafeteria.js
@@ -0,0 +1,16 @@
+console.log("¡Cargando contenido de la cafeteria!");
+
+const productos = [
+    {
+        Nombre:"Café de la Finca",Descripcion:"Nuestro café de origen único, cultivado en las montañas de Risaralda con notas a chocolate y panela",imagen:"favicon/cafe_negro.jpg"
+    },
+    {
+        Nombre:"Torta de Chocolate de la Abuela",Descripcion:"La receta secreta de la familia. Un bizcocho húmedo y una cobertura de chocolate que te hará feliz.",imagen:"favicon/torta_de_chocolate.png"
+    },
+    {
+        Nombre:"Arequipe Latte",Descripcion:"Una mezcla dulce y cremosa de espresso, leche vaporizada y un toque generoso de arequipe local.",imagen:"favicon/vaso_late.jpg"
+    },
+    {
+        Nombre:"Pandebono Relleno",Descripcion:"Nuestro pandebono tradicional, horneado al momento y relleno de queso mozzarella derretido.",imagen:"favicon/pandebono.png"
+    }
+]
