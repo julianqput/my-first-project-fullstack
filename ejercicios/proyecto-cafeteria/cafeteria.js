0db0704d566a83b25e6d9ad92405e27b7e0a4454
erlin julian quesada pinzon <quesadajulian39@gmail.com>
2025-10-08 10:40:39 -0500

    feat: se corrigio el array por que las claves estaban en mayusculas, se termino el js del archivo ya esta completo, solo falta terminar el csss

--- ejercicios/proyecto-cafeteria/cafeteria.js
+++ ejercicios/proyecto-cafeteria/cafeteria.js
@@ -2,15 +2,37 @@ console.log("¡Cargando contenido de la cafeteria!");
 
 const productos = [
     {
-        Nombre:"Café de la Finca",Descripcion:"Nuestro café de origen único, cultivado en las montañas de Risaralda con notas a chocolate y panela",imagen:"favicon/cafe_negro.jpg"
+        nombre:"Café de la Finca",
+        descripcion:"Nuestro café de origen único, cultivado en las montañas de Risaralda con notas a chocolate y panela",
+        imagen:"favicon/cafe_negro.jpg"
     },
     {
-        Nombre:"Torta de Chocolate de la Abuela",Descripcion:"La receta secreta de la familia. Un bizcocho húmedo y una cobertura de chocolate que te hará feliz.",imagen:"favicon/torta_de_chocolate.png"
+        nombre:"Torta de Chocolate de la Abuela",
+        descripcion:"La receta secreta de la familia. Un bizcocho húmedo y una cobertura de chocolate que te hará feliz.",
+        imagen:"favicon/torta_de_chocolate.png"
     },
     {
-        Nombre:"Arequipe Latte",Descripcion:"Una mezcla dulce y cremosa de espresso, leche vaporizada y un toque generoso de arequipe local.",imagen:"favicon/vaso_late.jpg"
+        nombre:"Arequipe Latte",
+        descripcion:"Una mezcla dulce y cremosa de espresso, leche vaporizada y un toque generoso de arequipe local.",
+        imagen:"favicon/vaso_late.jpg"
     },
     {
-        Nombre:"Pandebono Relleno",Descripcion:"Nuestro pandebono tradicional, horneado al momento y relleno de queso mozzarella derretido.",imagen:"favicon/pandebono.png"
+        nombre:"Pandebono Relleno",
+        descripcion:"Nuestro pandebono tradicional, horneado al momento y relleno de queso mozzarella derretido.",
+        imagen:"favicon/pandebono.png"
     }
 ]
+
+const elementoImage = document.querySelector('#producto-imagen');
+const elementoNombre = document.querySelector('#producto-nombre');
+const botonCambiar = document.querySelector('#cambiar-especial');
+
+function cambiarEspecial (){
+    const indiceAleatorio = Math.floor(Math.random()*productos.length);
+    const especialSeleccionado = productos[indiceAleatorio];
+    elementoImage.src = especialSeleccionado.ImagenSrc;
+    elementoNombre.textContent = especialSeleccionado.nombre;
+
+}
+
+botonCambiar.addEventListener('click', cambiarEspecial);
